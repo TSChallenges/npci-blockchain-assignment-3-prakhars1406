ac6a96e3a1dc31f02b4b05a6e84a21936dfb5cac
Prakhar Sharma <prakhars1406@gmail.com>
2025-01-07 13:16:32 +0000

    assignement 3 submission

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -16,29 +16,46 @@ contract ERC20Token {
     constructor(uint256 _initialSupply) {
         // TODO: Initialize totalSupply and assign it to the deployer's balance.
         // HINT: Use `_initialSupply` and the `decimals` variable.
+        
+        require(_initialSupply > 0, "Value should be greater than 0");
+        totalSupply = _initialSupply;
+        balances[msg.sender] = _initialSupply * (10 ** decimals);
     }
 
     function balanceOf(address account) public view returns (uint256) {
         // TODO: Return the balance of the given `account`.
         // HINT: Use the `balances` mapping.
+        
+        return balances[account];
     }
 
     function transfer(address to, uint256 value) public returns (bool) {
         require(balances[msg.sender] >= value, "Insufficient balance");
         // TODO: Implement the transfer logic.
         // HINT: Deduct `value` from sender's balance and add it to the recipient's balance.
+
+        require(value > 0, "Value should be greater than 0");
+        balances[msg.sender] = balances[msg.sender] - value;
+        balances[to] = balances[to] - value;
+
         emit Transfer(msg.sender, to, value);
         return true;
     }
 
     function approve(address spender, uint256 value) public returns (bool) {
         // TODO: Set the allowance for `spender` to spend `value` on behalf of the caller.
+
+        require(value > 0, "Value should be greater than 0");
+        allowances[msg.sender][spender] = value;
+
         emit Approval(msg.sender, spender, value);
         return true;
     }
 
     function allowance(address owner, address spender) public view returns (uint256) {
-        // TODO: Return the allowance set by `owner` for `spender`.
+        // TODO: Return the allowance set by `owner` for `spender`.        
+        
+        return allowances[owner][spender];
     }
 
     function transferFrom(address from, address to, uint256 value) public returns (bool) {
@@ -46,6 +63,11 @@ contract ERC20Token {
         require(allowances[from][msg.sender] >= value, "Allowance exceeded");
         // TODO: Implement the logic for transferring tokens on behalf of another address.
         // HINT: Update the `balances` and `allowances` mappings.
+
+        balances[from] = balances[from] - value;
+        balances[to] = balances[to] + value;
+        allowances[from][msg.sender] = allowances[from][msg.sender] - value;
+
         allowances[from][msg.sender] -= value;
         emit Transfer(from, to, value);
         return true;
@@ -53,12 +75,22 @@ contract ERC20Token {
 
     function mint(uint256 value) public {
         // TODO: Increase `totalSupply` by `value` and add `value` to the caller's balance.
+        
+        require(value > 0, "Value should be greater than 0");
+        totalSupply = totalSupply + value;
+        balances[msg.sender] = balances[msg.sender] + value;
+
         emit Transfer(address(0), msg.sender, value);
     }
 
     function burn(uint256 value) public {
         require(balances[msg.sender] >= value, "Insufficient balance");
         // TODO: Decrease `totalSupply` by `value` and subtract `value` from the caller's balance.
+
+        require(value > 0, "Value should be greater than 0");
+        totalSupply = totalSupply - value;
+        balances[msg.sender] = balances[msg.sender] - value;
+
         emit Transfer(msg.sender, address(0), value);
     }
 }
